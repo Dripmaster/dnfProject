fa50b994feb26b93f686a841b7232264de31f287
BaekMin Seo <seobm7996@gmail.com>
2020-03-22 00:14:04 +0900

    타이틀 화면 개발

---
+++ Assets/Scripts/title/FadeOut.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeOut : MonoBehaviour
+{
+    public float speed;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator RunFadeOut()
+    {
+        float frame = 10;
+
+        Color color = spriteRenderer.color;
+        float diffColorA = color.a / (speed * frame);
+        float waitSecond = speed / (speed * frame);
+
+
+        while (color.a > 0.0f)
+        {
+            color.a -= diffColorA;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(waitSecond);
+        }
+    }
+}
---
+++ Assets/Scripts/title/UpDownWave.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpDownWave : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+
+    bool isUp = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator RunWave()
+    {
+        float frame = 30;
+        float upTarget = transform.position.y + distance;
+        float originPosY = transform.position.y;
+
+        while (true)
+        {
+            if (isUp)
+                transform.position = new Vector2(transform.position.x, transform.position.y + (distance / (speed * frame)));
+            else
+                transform.position = new Vector2(transform.position.x, transform.position.y - (distance / (speed * frame)));
+
+            if (isUp && transform.position.y >= upTarget)
+            {
+                isUp = false;
+            }
+            if (!isUp && transform.position.y <= originPosY)
+                isUp = true;
+
+
+            yield return new WaitForSeconds(speed / (speed * frame));
+        }
+    }
+}
