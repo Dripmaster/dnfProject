6a5538f730358761c0fac0ee33b4037aefa9c325
BaekMin Seo <seobm7996@gmail.com>
2020-04-14 20:11:15 +0900

    맵 선택선택화면 마우스 이벤트 추가

---
+++ Assets/Scripts/selectScene/MouseEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvent : MonoBehaviour
+{
+    public Sprite hoverImage;
+    Sprite originImage;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        originImage = gameObject.GetComponent<SpriteRenderer>().sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnMouseEnter()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = hoverImage;
+    }
+    private void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = originImage;
+    }
+}
